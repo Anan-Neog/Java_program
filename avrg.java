479315652db831c961c69a1f3c0a4ffcc8c65fa9
Anan-Neog <125372335+Anan-Neog@users.noreply.github.com>
2023-08-29 11:28:33 +0530

    Add files via upload

---
+++ avrg.java
@@ -0,0 +1,16 @@
+package class_work;
+
+public class avrg {
+	public static void main(String[] args) {
+		int arr[]= {1,2,3,4,5};
+		int i,avg=0;
+		for (i=0;i<5;i++) {
+			avg=avg+arr[i];
+			
+		}
+		System.out.println("The average of is :"+avg);
+		
+
+	}
+	
+}
